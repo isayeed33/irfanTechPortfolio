909e51415394b3547081bf46000cfde2d479a92c
Irfan Sayeed <isayeed33@gmail.com>
2020-10-31 22:29:56 +0530

    Added Navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navibar from "./components/navbar/navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navibar></Navibar>
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;
